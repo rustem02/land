b5bf95ff2e849ff7e4c14325c88c28eab53d3765
krasav4ik01 <200103424@stu.sdu.edu.kz>
2022-06-30 18:01:09 +0600

    "ZBS"

--- main/views.py
+++ main/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'main/index.html')
+
+def form(request):
+    return render(request,'main/form.html')
+def example(request):
+    return render(request,'main/example.html')
