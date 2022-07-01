5d9e28118f5fb2a6b9cdd2e02d421fe917d7d282
krasav4ik01 <200103424@stu.sdu.edu.kz>
2022-07-01 17:53:10 +0600

    "last commit"

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('form/', views.form, name='form'),
     path('example/', views.example, name='example'),
 
