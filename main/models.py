9e9815559411e8d646b01b91cad805fc7dbaddde
Приб <pribka@gmail.com>
2022-07-03 09:43:34 +0600

    eee

--- main/models.py
+++ main/models.py
@@ -30,6 +30,7 @@ class Profile(models.Model):
 
 
 class Questionaire(models.Model):
+    choices = ((True, 'Да'), (False, 'Нет'),)
     created = models.DateTimeField(auto_now_add=True,
                                    verbose_name='Дата записи')
     email = models.EmailField(blank=False,
@@ -72,8 +73,8 @@ class Questionaire(models.Model):
         verbose_name="Есть ли собственные разработки (краткое описание)")
     company_personal = models.TextField(blank=False,
                                         verbose_name="Количество сотрудников \ из них продавцов \ из них инженеров в фирме ?")
-    company_ceo_has_tech = models.BooleanField(verbose_name='Имеет ли управляющий (директор) техническое образование?')
-    company_has_filial = models.BooleanField(verbose_name='Есть ли у Вашего предприятия филиалы в других регионах РФ? ')
+    company_ceo_has_tech = models.BooleanField(choices=choices,  verbose_name='Имеет ли управляющий (директор) техническое образование?')
+    company_has_filial = models.BooleanField(choices=choices,verbose_name='Есть ли у Вашего предприятия филиалы в других регионах РФ? ')
     company_regions = models.TextField(blank=True, default='',
         verbose_name="Регионы, где есть филиал")
     company_destination_activity = models.TextField(blank=False,
@@ -81,26 +82,26 @@ class Questionaire(models.Model):
 
     company_different_services = models.TextField(blank=True, default='',
         verbose_name="Дополнительные сервисы компании")
-    prom_kipia = models.BooleanField(verbose_name='Промышленная автоматика и КИПиА, в т.ч.')
-    non_contact = models.BooleanField(verbose_name='Бесконтактные датчики')
-    optical = models.BooleanField(verbose_name='Оптические датчики')
-    indikator = models.BooleanField(verbose_name='Управление и индикация')
-    temperature = models.BooleanField(verbose_name='Датчики давления и температуры')
-    ibp = models.BooleanField(verbose_name='Блоки питания')
-    encoder = models.BooleanField(verbose_name='Энкодеры')
-    converter = models.BooleanField(verbose_name='Частотные преобразователи')
-    motor_reductor = models.BooleanField(verbose_name='Мотор-редукторы и электродвигатели')
-    baumer = models.BooleanField(
+    prom_kipia = models.BooleanField(choices=choices,verbose_name='Промышленная автоматика и КИПиА, в т.ч.')
+    non_contact = models.BooleanField(choices=choices,verbose_name='Бесконтактные датчики')
+    optical = models.BooleanField(choices=choices,verbose_name='Оптические датчики')
+    indikator = models.BooleanField(choices=choices,verbose_name='Управление и индикация')
+    temperature = models.BooleanField(choices=choices,verbose_name='Датчики давления и температуры')
+    ibp = models.BooleanField(choices=choices,verbose_name='Блоки питания')
+    encoder = models.BooleanField(choices=choices,verbose_name='Энкодеры')
+    converter = models.BooleanField(choices=choices,verbose_name='Частотные преобразователи')
+    motor_reductor = models.BooleanField(choices=choices,verbose_name='Мотор-редукторы и электродвигатели')
+    baumer = models.BooleanField(choices=choices,
         verbose_name='Датчики Baumer (Швейцария/Германия): оптические, лазерные, ультразвуковые, касания, индуктивные, емкостные')
-    process_baumer = models.BooleanField(
+    process_baumer = models.BooleanField(choices=choices,
         verbose_name='Датчики процессов для пищевых производств Baumer (уровня, давление, температуры, концентрации) в пищевом исполнении')
-    cement_pesok_zerno = models.BooleanField(
+    cement_pesok_zerno = models.BooleanField(choices=choices,
         verbose_name='Датчики уровня сыпучих материалов (песок, цемент, зерно, комбикорм)')
-    convair_security = models.BooleanField(
+    convair_security = models.BooleanField(choices=choices,
         verbose_name='Конвейерная безопасность (аварийный выключатель конвейера, датчик схода ленты, датчик препятствия, датчик затора, датчик перегруза)')
-    light_indication = models.BooleanField(
+    light_indication = models.BooleanField(choices=choices,
         verbose_name='Светосигнальное оборудование (светосигнальные колонны и маячки)')
-    other_device = models.BooleanField(verbose_name='Другое')
+    other_device = models.BooleanField(choices=choices,verbose_name='Другое')
     other_device_description = models.TextField(blank=True, default='',verbose_name='Описание иного оборудования')
     company_can_have_dealer_page = models.TextField(blank=False,
                                                     verbose_name="Готовы ли Вы освоить Личный кабинет дилера для самостоятельной работы с клиентом после заключения договора в течении 3-4 дней?")
--- main/views.py
+++ main/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import *
+from django.core.mail import EmailMultiAlternatives
 
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+from django.db.models.fields import BooleanField
+def render_table(instans):
+    l = []
+
+    for each in instans._meta.fields:
+        val = getattr(instans, each.name)
+        if type(each) == BooleanField:
+            if getattr(instans, each.name):
+                val = 'Да'
+            else:
+                val = "Нет"
+        l.append({'n': each.verbose_name, 'v': val})
+
+    return l
 
 
 def form(request):
@@ -14,9 +30,28 @@ def form(request):
         fm = QuestionareForm(request.POST)
 
         if fm.is_valid():
-            #captcha_score = fm.cleaned_data['captcha'].get('score')
             fm.save()
-            return render(request, 'main/index.html')
+
+            subject, from_email, to = 'Новая заявка на дилерство', 'dealer@prst.ru', 'pribka@mail.ru'
+            text_content = 'Новая заявка на дилерство'
+            cnt = render_table(fm.instance)
+            html_content = render(request, 'main/formemail.html', context={'cnt': cnt}).content
+            email = EmailMultiAlternatives(subject, text_content, from_email, [to], )
+            email.attach_alternative(html_content, 'text/html')
+            email.send()
+
+
+
+        else:
+            subject, from_email, to = 'Ошибка в заявке на дилерство', 'dealer@prst.ru', 'pribka@mail.ru'
+            text_content = 'Ошибка в заявке на дилерство'
+
+            html_content = render(request, 'main/formemail.html', context={'fm': fm}).content
+            email = EmailMultiAlternatives(subject, text_content, from_email, [to], )
+            email.attach_alternative(html_content, 'text/html')
+            email.send()
+
+        return render(request, 'main/form_complete.html')
 
     return render(request, 'main/form.html', context={'fm': fm})
 
