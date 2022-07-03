ad3e70414ab0e472b5f18954355daae3b1ed01e6
Приб <pribka@gmail.com>
2022-07-03 09:56:02 +0600

    eee

--- main/models.py
+++ main/models.py
@@ -73,8 +73,8 @@ class Questionaire(models.Model):
         verbose_name="Есть ли собственные разработки (краткое описание)")
     company_personal = models.TextField(blank=False,
                                         verbose_name="Количество сотрудников \ из них продавцов \ из них инженеров в фирме ?")
-    company_ceo_has_tech = models.BooleanField(choices=choices,  verbose_name='Имеет ли управляющий (директор) техническое образование?')
-    company_has_filial = models.BooleanField(choices=choices,verbose_name='Есть ли у Вашего предприятия филиалы в других регионах РФ? ')
+    company_ceo_has_tech = models.BooleanField(  verbose_name='Имеет ли управляющий (директор) техническое образование?')
+    company_has_filial = models.BooleanField(verbose_name='Есть ли у Вашего предприятия филиалы в других регионах РФ? ')
     company_regions = models.TextField(blank=True, default='',
         verbose_name="Регионы, где есть филиал")
     company_destination_activity = models.TextField(blank=False,
@@ -82,26 +82,26 @@ class Questionaire(models.Model):
 
     company_different_services = models.TextField(blank=True, default='',
         verbose_name="Дополнительные сервисы компании")
-    prom_kipia = models.BooleanField(choices=choices,verbose_name='Промышленная автоматика и КИПиА, в т.ч.')
-    non_contact = models.BooleanField(choices=choices,verbose_name='Бесконтактные датчики')
-    optical = models.BooleanField(choices=choices,verbose_name='Оптические датчики')
-    indikator = models.BooleanField(choices=choices,verbose_name='Управление и индикация')
-    temperature = models.BooleanField(choices=choices,verbose_name='Датчики давления и температуры')
-    ibp = models.BooleanField(choices=choices,verbose_name='Блоки питания')
-    encoder = models.BooleanField(choices=choices,verbose_name='Энкодеры')
-    converter = models.BooleanField(choices=choices,verbose_name='Частотные преобразователи')
-    motor_reductor = models.BooleanField(choices=choices,verbose_name='Мотор-редукторы и электродвигатели')
-    baumer = models.BooleanField(choices=choices,
+    prom_kipia = models.BooleanField(verbose_name='Промышленная автоматика и КИПиА, в т.ч.')
+    non_contact = models.BooleanField(verbose_name='Бесконтактные датчики')
+    optical = models.BooleanField(verbose_name='Оптические датчики')
+    indikator = models.BooleanField(verbose_name='Управление и индикация')
+    temperature = models.BooleanField(verbose_name='Датчики давления и температуры')
+    ibp = models.BooleanField(verbose_name='Блоки питания')
+    encoder = models.BooleanField(verbose_name='Энкодеры')
+    converter = models.BooleanField(verbose_name='Частотные преобразователи')
+    motor_reductor = models.BooleanField(verbose_name='Мотор-редукторы и электродвигатели')
+    baumer = models.BooleanField(
         verbose_name='Датчики Baumer (Швейцария/Германия): оптические, лазерные, ультразвуковые, касания, индуктивные, емкостные')
-    process_baumer = models.BooleanField(choices=choices,
+    process_baumer = models.BooleanField(
         verbose_name='Датчики процессов для пищевых производств Baumer (уровня, давление, температуры, концентрации) в пищевом исполнении')
-    cement_pesok_zerno = models.BooleanField(choices=choices,
+    cement_pesok_zerno = models.BooleanField(
         verbose_name='Датчики уровня сыпучих материалов (песок, цемент, зерно, комбикорм)')
-    convair_security = models.BooleanField(choices=choices,
+    convair_security = models.BooleanField(
         verbose_name='Конвейерная безопасность (аварийный выключатель конвейера, датчик схода ленты, датчик препятствия, датчик затора, датчик перегруза)')
-    light_indication = models.BooleanField(choices=choices,
+    light_indication = models.BooleanField(
         verbose_name='Светосигнальное оборудование (светосигнальные колонны и маячки)')
-    other_device = models.BooleanField(choices=choices,verbose_name='Другое')
+    other_device = models.BooleanField(verbose_name='Другое')
     other_device_description = models.TextField(blank=True, default='',verbose_name='Описание иного оборудования')
     company_can_have_dealer_page = models.TextField(blank=False,
                                                     verbose_name="Готовы ли Вы освоить Личный кабинет дилера для самостоятельной работы с клиентом после заключения договора в течении 3-4 дней?")
